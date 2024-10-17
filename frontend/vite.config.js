d916b64c750b0af23922522fe1a579933519da98
Dev Juu <devjuu@MacBook-Pro-khxng-Dev.local>
2024-10-17 17:04:44 +0700

    Update vite.config.js for Netlify deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ตั้งค่า base ให้เป็นแบบ relative สำหรับ assets
   plugins: [react()],
+  build: {
+    outDir: 'dist', // กำหนดโฟลเดอร์สำหรับผลลัพธ์การ build (Netlify จะใช้โฟลเดอร์นี้ในการ deploy)
+    rollupOptions: {
+      input: './index.html' // กำหนด entry point เป็น index.html
+    }
+  },
+  server: {
+    port: 3000, // ตั้งค่าพอร์ตของเซิร์ฟเวอร์ระหว่างการพัฒนา
+    open: true // เปิดเบราว์เซอร์อัตโนมัติเมื่อรันเซิร์ฟเวอร์พัฒนา
+  }
 })
